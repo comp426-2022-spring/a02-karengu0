f2bfcc30c76e01631fd09254735f032b280475b3
Karen Guo <karengu0@live.unc.edu>
2022-02-22 12:34:13 -0500

    guess flip

--- guess-flip.js
+++ guess-flip.js
@@ -1,14 +1,15 @@
-import { createRequire } from 'module';
+import minimist from 'minimist';
+//import { createRequire } from 'module';
 import { flipACoin } from './modules/coin.mjs';
-const require = createRequire(import.meta.url);
+//const require = createRequire(import.meta.url);
 
-const minimist = require('minimist')
-var argument = minimist(process.argv.slice(2));
+//const minimist = require('minimist')
+const argument = minimist(process.argv.slice(2))
 
 if (argument.call == "heads" || argument.call == "tails") {
     console.log(flipACoin(argument.call));
 }
 
 else {
-    console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]")
+    console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 }
